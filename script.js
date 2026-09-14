398a9a75a34f6b0575dc937eca6f1524e90aec44
João Gabriel <joaogabrieloliver4@gmail.com>
2026-09-14 11:18:55 -0300

    abrindo o whatsapp

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let botaoWhats = document.querySelector(".chat-toggle");
+
 let servicos = document.querySelectorAll(".servico");
 console.log("Lista de serviços:", servicos);
 console.log("Primeiro serviços:", servicos[0]);
@@ -7,3 +8,13 @@ console.log("Segundo serviços:", servicos[1]);
 function mostrarMensagem() {
     alert("Você ciclou em um serviço!");
 }
+
+let primeiroServico = document.querySelector(".servico");
+primeiroServico.addEventListener("click", mostrarMensagem);
+
+function abrirWhats(e) {
+    e.preventDefault();
+    alert("Você será redirecionado para o WhatsApp!");
+    window.open("Https://wa.me/5586999327053", "_blank")
+}
+botaoWhats.addEventListener("click", abrirWhats);
