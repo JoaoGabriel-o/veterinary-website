4c7badf5a4ee21e77630d31340262e971e70b3fe
João Gabriel <joaogabrieloliver4@gmail.com>
2026-09-14 11:07:34 -0300

    atualização da seção eventos

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let servicos = document.querySelectorAll(".servico");
 console.log("Lista de serviços:", servicos);
 console.log("Primeiro serviços:", servicos[0]);
 console.log("Segundo serviços:", servicos[1]);
+
+function mostrarMensagem() {
+    alert("Você ciclou em um serviço!");
+}
